de1881635b687599d8d1c22229029a60641e4f39
Emmanuel Okororie <manuelokororie@gmail.com>
2025-08-19 09:18:27 +0100

    add readme

--- vars.tf
+++ vars.tf
@@ -1,9 +1,3 @@
-# variable "netlify_token" {
-#   description = "Netlify personal access token. Set in HCP workspace as env NETLIFY_TOKEN."
-#   type        = string
-#   sensitive   = true
-# }
-
 variable "site_name" {
   description = "Optional custom Netlify subdomain. Random if null."
   type        = string
